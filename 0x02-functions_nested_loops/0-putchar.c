24d76844c0d9d1302570014ae8093817224546c1
Tonyrealzy <umehobiarinze2@gmail.com>
2022-10-19 20:51:13 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,7 +5,7 @@
  * Return: 0
  */
 
-int main(void)
+int main(void);
 {
 	_putchar('_');
 	_putchar('p');
@@ -16,6 +16,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-
 	return (0);
 }
