225c436c4cb6550063e49cab26bec6594e43763a
Tonyrealzy <umehobiarinze2@gmail.com>
2022-10-23 03:16:31 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+
+/**
+ * main - prints the alphabet in lowercase in reverse
+ * followed by a new line
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	
+	char letter;
+	
+	for (letter = 'z'; letter >= 'a'; letter--)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	
+	return (0);
+
+}
