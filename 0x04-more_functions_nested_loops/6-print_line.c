b2905c037a1d2e4f895417036913f28369f00bf1
Tonyrealzy <umehobiarinze2@gmail.com>
2022-10-24 13:48:45 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,7 +3,7 @@
 
 /**
  * print_line - draws a straight line
- *@n: number of times
+ * @n: number of times
  * 
  * Return: void
  */
@@ -12,7 +12,7 @@ void print_line(int n)
 {
 	int i;
 
-	for (i = '0'; i <= 'n'; i++)
+	for (i = '0'; i <= n; i++)
 		putchar('_');
 	putchar('\n');
 }
