0f2f91169bd06eeaf51acf71f5314d2f9e5e7eb2
Tonyrealzy <umehobiarinze2@gmail.com>
2022-10-24 22:29:31 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - return the length of a string
+ * @s: char to check
+ *
+ * Return: 0 is success
+ */
+
+int _strlen(char *s)
+{
+	int a = 0;
+	
+	for (s > 0; *s++)
+		a++;
+	return (a);
+
+}
+
