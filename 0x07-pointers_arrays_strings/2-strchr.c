6e9b45a8da6f037d57c32d937c55905b0fe71e5f
Tonyrealzy <umehobiarinze2@gmail.com>
2022-11-11 11:46:57 +0100

    yea yea

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,10 +4,11 @@
  * _strchr - locates a character in a string
  * @s: string
  * @c: character
- * Return: pointer to c or NULL
+ * Return: pointer to c
  */
 
 char *_strchr(char *s, char c)
+
 {
 	char *p;
 	int i;
