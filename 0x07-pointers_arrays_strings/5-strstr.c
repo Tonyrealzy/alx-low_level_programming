72f684f695445e915d46afd59460f1c0e2103e66
Tonyrealzy <umehobiarinze2@gmail.com>
2022-11-11 14:54:32 +0100

    yea yea

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strstr -  locates a substring
+ * @haystack: string
+ * @needle: substring
+ * Return: a pointer to the located substring
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack)
+	{
+		char *h = haystack;
+		char *n = needle;
+		while (*haystack && *n == *n)
+		{
+			*haystack++;
+			*n++;
+		}
+		if (!*n)
+		{
+			return (h);
+		}
+		haystack = h + 1;
+	}
+	return (0);
+}
---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_diagsums - prints the sum of the two diagonals 
+ * of a square matrix of integers
+ * @a: pointer to array
+ * @size: size of array
+ * Return: void
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, size1;
+	unsigned int Diag1, Diag2;
+
+	size1 = 0;
+	Diag1 = 0;
+	Diag2 = 0;
+
+	size1 = (size * size) - 1;
+
+	for (i = 0; i <= size1; i = i + (size + 1))
+	{
+		Diag1 = Diag1 + a[i];
+	}
+	for (i = (size - 1); i < size1; i = i + (size - 1))
+	{
+		 Diag2 = Diag2 + a[i];
+	}
+	printf("%d, %d\n", Diag1, Diag2);
+}
