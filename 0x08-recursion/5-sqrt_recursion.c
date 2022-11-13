a8a58efda8caaa5f3164a6887098782a77f63eaa
Tonyrealzy <umehobiarinze2@gmail.com>
2022-11-13 12:33:21 +0100

    Sunday

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,29 @@
 #include "main.h"
 
 /**
+ * helperFunction - checks if square root
+ * of a number exists
+ * @num: number to check
+ * @p: the square root which exists
+ * Return: square root of num or -1
+ */
+
+int helperFunction(int num, int p)
+{
+	if (p * p == num)
+	{
+		return (p);
+	}
+	else
+	{
+		if (p * p > num)
+			return (-1);
+		else 
+			return (helperFunction(num, p + 1));
+	}
+}
+
+/**
  * _sqrt_recursion - returns the natural
  * square root of a number
  * @n: number to get square root
@@ -9,5 +32,8 @@
 
 int _sqrt_recursion(int n)
 {
-
+	if (n < 0)
+		return (-1);
+	else
+		return (helperFunction(n, 0));
 }
