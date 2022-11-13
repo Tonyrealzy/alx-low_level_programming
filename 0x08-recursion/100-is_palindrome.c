ee33598646d4bd08a9d9e6f4598fd404d128f985
Tonyrealzy <umehobiarinze2@gmail.com>
2022-11-14 00:35:45 +0100

    Another recursion task

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,57 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns length of string
+ * @s: pointer to string
+ * Return: length of string
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s)
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	return (0);
+}
+
+/**
+ * helperFunction - checks if palindrome
+ * @s: pointer to string
+ * @i1: first index
+ * @i2: second index
+ * Return: 0 or 1
+ */
+
+int helperFunction(char *s, int i1, int i2)
+{
+	if (i1 < i2 && s[i1] == s[i2])
+	{
+		return (helperFunction(s, i1 + 1, i2 - 1));
+	}
+	if (s[i1] != [i2])
+	{
+		return (0);
+	}
+	return (1);
+}
+
+/**
+ * is_palindrome - returns 1 if a
+ * string is a palindrome and 0 if not
+ * @s: string
+ * Return: 0 or 1
+ */
+
+int is_palindrome(char *s)
+{
+	int i1, i2;
+
+	i1 = 0;
+	i2 = _strlen_recursion(s) - 1;
+
+	if (!*s)
+		return (1);
+
+	return (helperFunction(s, i1, i2));
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * helperFunction - returns 0 or 1
+ * @num: number being checked
+ * @i: possible factor num
+ * Return: 1, if prime or 0 if not
+ */
+
+int helperFunction(int num, int i)
+{
+	if (i < num)
+	{
+		if (num % i == 0)
+			return (0);
+		else
+			return (helperFunction(num, i + 1));
+	}
+	else 
+		return (1);
+}
+
+
+/**
+ * is_prime_number - checks if input is prime
+ * @n: number to check
+ * Return: 1 or 0
+ */
+
+int is_prime_number(int n)
+{
+	if (n == 0)
+		return (0);
+	else
+		return (helperFunction(n, 2));
+}
