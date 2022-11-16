079539c688ae37a707616619054c245eea2f62de
Tonyrealzy <umehobiarinze2@gmail.com>
2022-11-16 21:46:07 +0100

    Create 3-strspn.c

---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: string
+ * @accept: contains bytes
+ * Return: number of bytes
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int n = 0, i, j;
+
+	i = 0;
+	while (s[i] != '0')
+	{
+		if (s[i] != 32)
+		{
+			j = 0;
+			while (accept[j] != '0')
+			{
+				if (s[i] == accept[j])
+				{
+					n += 1;
+				}
+				j++;
+			}
+		}
+		else
+			return (n);
+		i++;
+	}
+	return (n);
+}
