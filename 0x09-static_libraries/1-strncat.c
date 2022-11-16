390db7661eb68ea386e73802e2c84e5f7f300ea9
Tonyrealzy <umehobiarinze2@gmail.com>
2022-11-16 21:35:58 +0100

    Create 1-strncat.c

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings
+ *
+ * @src: The source of strings
+ * @dest: The destination of the string
+ * @n: The length of int
+ *
+ * Return: pointer to the resulting string dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int a, j;
+
+	for (a = 0; dest[a] != '\0'; a++)
+	{
+		continue;
+	}
+	for (j = 0; src[j] != '\0' && j < n; j++)
+	{
+		dest[a + j] = src[j];
+	}
+	dest[a + j] = '\0';
+	return (dest);
+}
