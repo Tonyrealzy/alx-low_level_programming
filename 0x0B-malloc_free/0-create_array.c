a4185c97265b74d0724161f91df9c5f786deaa47
Tonyrealzy <umehobiarinze2@gmail.com>
2022-11-18 12:44:45 +0100

    0x0B. C - malloc, free tasks

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of chars
+ * and initializes it with a specific char
+ * size: size of array
+ * c: constant
+ * Return: pointer to the array, or NULL if it fails
+ */ 
+
+char *create_array(unsigned int size, char c)
+{
+	char *p;
+	unsigned int i;
+
+	if size == 0
+		return (NULL);
+
+	p = malloc(size * sizeof(*p));
+	if p == 0
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		p[i] = c;
+	return (p);
+}
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup -  returns a pointer to a newly allocated space in
+ * memory which contains a copy of the string given as a parameter
+ * @str: string
+ * Return: NULL or pointer to str
+ */
+
+char *_strdup(char *str)
+{
+	char *p;
+	int i, j;
+
+	if str == NULL
+		return (NULL);
+
+	while (i = 0; str[i] != '\0')
+	{
+		i++;
+	}
+	p = malloc(i * sizeof(*p) + 1);
+	if (p = NULL)
+		return (NULL);
+
+	for (c = 0; c < i; c++)
+		p[c] = str[c];
+	p[c] != '\0';
+	return (p);
+}
