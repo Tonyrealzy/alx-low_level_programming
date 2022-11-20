4bfdef2cf94f98822357f364c2b36198b4d394d0
Tonyrealzy <umehobiarinze2@gmail.com>
2022-11-20 11:13:48 +0100

    0x0C. C - More malloc, free tasks

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -35,9 +35,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		p[i] = s1[i];
 	}
 
-	while (i < (z1 + n))
+	for (;i < (z1 + n); i++)
 	{
-		i++;
 		p[i] = s2[i - z1];
 	}
 	p[i] = '\0';
