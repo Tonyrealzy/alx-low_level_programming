17d44eddbc052c8989e2862d0bb055266902ce15
Tonyrealzy <umehobiarinze2@gmail.com>
2022-11-30 17:25:55 +0100

    0-object_like_macro.h

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 
 /**
-main - prints the name of the file it was
-compiled from, followed by a new line
+ * main - prints the name of the file it was
+ * compiled from, followed by a new line
 
-Return: 0
-*/
+ * Return: 0
+ */
 
 int main(void)
 {
