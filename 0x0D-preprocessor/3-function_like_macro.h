065e9199a2554b98ad5bd8ad7b523fd097709b6b
Tonyrealzy <umehobiarinze2@gmail.com>
2022-11-30 17:15:36 +0100

    0-object_like_macro.h

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,5 @@
+#ifndef ABS(MACRO_H)
+#define ABS(MACRO_H)
+
+#define ABS(x) ((x) < 0 ? (-(x)) : (x))
+#endif
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,5 @@
+#ifndef SUM_OF_X_Y
+#define SUM_OF_X_Y
+
+#define SUM(x, y) ((x) + (y))
+#endif
