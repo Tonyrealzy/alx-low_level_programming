7f738f0b2d6099adec9607d526050eb8396a8fee
Tonyrealzy <umehobiarinze2@gmail.com>
2022-12-04 09:44:20 +0100

    My second commit

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees dogs
+ * @dog_t: new name of struct dog
+ * @d: pointer
+ *
+ * Return: void
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+
+		free(d);
+	}
+}
