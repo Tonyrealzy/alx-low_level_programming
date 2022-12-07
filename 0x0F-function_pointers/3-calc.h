ac18d0653d67a0516ecfe002d12a6dbdb1303476
Tonyrealzy <umehobiarinze2@gmail.com>
2022-12-07 07:28:53 +0100

    C - Function pointers tasks

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int op_add(int a, int b);
 int op_sub(int a, int b);
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,41 @@
+#include "3-calc.h"
+
+/**
+ * main - checks argument
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: int
+ */
+
+int main(int argc, char *argv[])
+{
+	char s;
+	int a, b, res;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	/*checking for an extra operator*/
+	if (argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	
+	/*Usage: calc num1 operator num2*/
+	s = argv[2][0];
+	if (s != '+' && s != '-' && s != '*' && s != '/' && s != '%')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	res = (get_op_func(argv[2]))(a, b);
+	printf("%d\n", res);
+	return (0);
+}
