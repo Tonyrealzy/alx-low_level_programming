2fb411f33903a84f8890ff3641917c6543c0720b
Tonyrealzy <umehobiarinze2@gmail.com>
2022-12-07 14:00:22 +0100

    C - Function pointers tasks

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -19,13 +19,13 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 	/*checking for an extra operator*/
+	s = argv[2][0];
 	if ((argv[2][1] != '\0') || (s != '+' && s != '-' && s != '*' && s != '/' && s != '%'))
 	{
 		printf("Error\n");
 		exit(99);
 	}
 	/*Usage: calc num1 operator num2*/
-	s = argv[2][0];
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 	if ((b == 0 && s == '/') || (b == 0 && s == '%'))
