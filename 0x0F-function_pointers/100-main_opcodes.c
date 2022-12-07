6af37d64239581b0bda3527a71e262a102c56c81
Tonyrealzy <umehobiarinze2@gmail.com>
2022-12-08 00:50:14 +0100

    Function pointers

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -6,19 +6,19 @@
  * @argc: argument count
  * @argv: argument vector
  *
- * Return: integer
+ * Return: int
  */
 
 int main(int argc, char *argv[])
 {
 	int i, n;
+	char *opc = (char *)main;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-	
 	/*format is: main number_of_bytes*/
 	n = atoi(argv[1]);
 	if (n < 0)
@@ -28,10 +28,11 @@ int main(int argc, char *argv[])
 	}
 	for (i = 0; i < n; i++)
 	{
-		printf("%02x\n", *((char *)main + i));
+		printf("%02x\n", opc[i]);
 		if (i < n - 1)
 			printf(" ");
 		else
 			printf("\n");
 	}
+	return (0);
 }
