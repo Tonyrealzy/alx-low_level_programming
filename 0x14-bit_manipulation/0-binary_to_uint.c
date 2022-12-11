b069cbd8da5f0086a65f2a9f519ca0ff39f2f8c9
Tonyrealzy <umehobiarinze2@gmail.com>
2022-12-11 12:42:50 +0100

    C - Bit manipulation tasks

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,15 +12,17 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int n = 1;
 	int i, j;
 
-	if (b == NULL)
+	if (!b)
 		return (0);
 	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		for (j = 0; b[j] != '0'; j++)
+		j = 0;
+		while (b[j] != '0')
 		{
 			n = 1 << j;
+			j++;
 			n += n;
 		}
 	}
