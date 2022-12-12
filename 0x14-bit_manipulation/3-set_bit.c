47f327b2c244ce74ff0880391142382da26d2374
Tonyrealzy <umehobiarinze2@gmail.com>
2022-12-12 16:53:14 +0100

    Bit manipulation tasks

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -11,10 +11,11 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int *p;
+	unsigned int *p;
 
 	if (index > 64)
 		return (-1);
+
 	p = (n >> index) & 1;
 	return (1);
 }
